02f78dcac3d1da7548c5926eccc54863d32a642f
Thirukumaran <114419114+Thiru-kumaran-R@users.noreply.github.com>
2026-05-23 13:36:04 +0530

    Completed Video - 2

--- Big Chill/Arrays/VIDEO-2.java
+++ Big Chill/Arrays/VIDEO-2.java
@@ -185,3 +185,116 @@ class Main {
 }
 
 // Optimal
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5,9};
+        int[] arr2 = {7, 3, 8 };
+        
+        Arrays.sort(arr);
+        Arrays.sort(arr2);
+        List<Integer> union = new ArrayList<>();
+        int size1 = arr.length;
+        int size2 = arr2.length;
+        int right = 0, left = 0;
+        
+        while(right < size1 && left < size2){
+            if(arr[right] <= arr2[left]){
+                if(union.size() > 0 && arr[right] == union.get(union.size() - 1)){
+                    continue;
+                }else{
+                    union.add(arr[right]);
+                }
+                right += 1;
+            }else{
+                if(union.size() > 0 && arr2[left] == union.get(union.size() - 1)){
+                    continue;
+                }else{
+                    union.add(arr2[left]);
+                }
+                left += 1;
+            }
+        }
+        
+        while(right < size1){
+            union.add(arr[right]);
+            right += 1;
+        }
+        
+        while(left < size2){
+            union.add(arr2[left]);
+            left += 1;
+        }
+        
+        System.out.println(union);
+    }
+}
+
+// Intersection of two sorted arrays
+// Brute Force
+
+class q1 {
+    public static void main(int[] arr, int[] arr2) {
+        List<Integer> union = new ArrayList<>();
+        Arrays.sort(arr);
+        Arrays.sort(arr2);
+        int size1 = arr.length;
+        int size2 = arr2.length;
+        int[] visited = new int[size2];
+        int right = 0, left = 0;
+        
+        for(int i = 0; i < size1; i++){
+            for(int j = 0; j < size2; j++){
+                if(arr[i] == arr2[j] && visited[j] == 0 && 
+                    (union.size() == 0 ||
+                    union.get(union.size() - 1) != arr[i])
+                ){
+                    union.add(arr[i]);
+                    visited[j] = 1;
+                    break;
+                }
+                
+                if(arr[i] < arr2[j]) break;
+            }
+        }
+        
+        return union.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
+
+// Optimal
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+import java.util.*;
+
+class q1 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5,9};
+        int[] arr2 = {7, 3, 2, 5, 6, 8 };
+        
+        Arrays.sort(arr);
+        Arrays.sort(arr2);
+        
+        List<Integer> intersection = new ArrayList<>();
+        
+        int left = 0, right = 0;
+        int size1 = arr.length, size2 = arr2.length;
+        
+        while(left < size1 && right < size2){
+            if(arr[left] == arr2[right]){
+                intersection.add(arr[left]);
+                left += 1;
+                right += 1;
+            }else if(arr[left] > arr2[right]){
+                right += 1;
+            }else{
+                left += 1;
+            }
+        }
+        
+        System.out.println(intersection);
+    }
+}
