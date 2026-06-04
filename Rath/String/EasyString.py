4c723151c0ca3fed64cf12a4f054bc99b831fa2e
Ruchika Yadav <145842561+ruchika123yadav@users.noreply.github.com>
2026-06-04 21:45:15 +0530

    solved 1

--- Rath/String/EasyString.py
+++ Rath/String/EasyString.py
@@ -263,7 +263,72 @@ class Solution {
 
 
 
+# LONGEST PALINDROME SUBSTRING->https://leetcode.com/problems/longest-palindromic-substring/description/
 
+# BRUTE FORCE COMPLEXITY=O(n^3)
+
+class Solution:
+
+    def palindrome(self,s:str)->bool:
+         i=0
+         j=len(s)-1
+         while i<j:
+            if s[i]!=s[j]:
+                return False
+            i+=1
+            j-=1
+         
+         return True
+
+
+    def longestPalindrome(self, ss: str) -> str:
+        # if(len(ss)==1):
+        #     return ""
+
+
+        result=""
+
+        for i in range(len(ss)):
+             
+            for j in range(i,len(ss)):
+                 
+                if(self.palindrome(ss[i:j+1])):
+                    if(len(result)<len(ss[i:j+1])):
+                        result=ss[i:j+1]
+
+        return result
+
+
+
+# OPTIMAL APPRAOCH COMPLEXITY=O(n^2)
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if not s:
+            return ""
+
+        def expand_around_center(s:str,left:int,right:int):
+            while left>=0 and right<len(s) and s[left]==s[right]:
+                left-=1
+                right+=1
+            return right-left-1
+
+
+        start=0
+        end=0
+
+        for i in range(len(s)):
+            odd=expand_around_center(s,i,i)
+            even=expand_around_center(s,i,i+1)
+            max_len=max(odd,even)
+
+            if max_len>end-start:
+                start=i-(max_len-1)//2
+                end=i+max_len//2
+
+        return s[start:end+1]
+
+        
 
         
 
