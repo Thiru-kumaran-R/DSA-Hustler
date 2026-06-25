cbd9cadeda16a610f14f03de4bed6216a8601a1c
Ruchika Yadav <145842561+ruchika123yadav@users.noreply.github.com>
2026-06-25 20:09:57 +0530

    solved potd

--- Rath/POTD/potd.py
+++ Rath/POTD/potd.py
@@ -265,6 +265,31 @@ class Solution:
         return res % mod
         
 
+
+# COUNT SUBARRAY WITH MAJORITY ELEMENT | ->https://leetcode.com/problems/count-subarrays-with-majority-element-i/?envType=daily-question&envId=2026-06-25
+# COMPLEXITY=O(n^2)
+
+class Solution:
+    def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
+
+        res=0
+
+        for i in range(len(nums)):
+            count=0
+            for j in range(i,len(nums)):
+                if nums[j]==target:
+                    count+=1
+
+                length=(j-i+1)
+
+                if 2*count>length:
+                    res+=1
+
+        
+        return res
+
+
+        
         
 
         
