e1ea3b1302d046631f7a49bbff783aa4f57cd371
Ruchika Yadav <145842561+ruchika123yadav@users.noreply.github.com>
2026-07-10 19:46:04 +0530

    solved 2 two P questions

--- Rath/SlidingWindow_TwoPointer/file.py
+++ Rath/SlidingWindow_TwoPointer/file.py
@@ -25,3 +25,72 @@ class Solution:
 
 
         return maxLength
+
+
+
+# MAX CONSECUTIVE ONES||| ->https://leetcode.com/problems/max-consecutive-ones-iii/description/
+
+# complexity=O(2n)
+# BETTER SOLUTION
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+
+        if len(nums)==1 and nums[0]==1:
+            return 1
+
+        right=0
+        left=0
+        maxLength=0
+        zero=0
+
+        while right<len(nums):
+            if nums[right]==0:
+                zero+=1
+
+            if zero>k:
+                while nums[left]!=0:
+                    left+=1
+                left+=1
+                zero-=1
+
+            maxLength=max(maxLength,right-left+1)
+
+            right+=1
+
+
+        return maxLength
+            
+
+# OPTIMAL SOLUTION-O(n)
+
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+
+        if len(nums)==1 and nums[0]==1:
+            return 1
+
+        right=0
+        left=0
+        maxLength=0
+        zero=0
+
+        while right<len(nums):
+            if nums[right]==0:
+                zero+=1
+
+            if zero>k:
+                if nums[left]==0:
+                    zero-=1
+                left+=1
+                         
+            if zero<=k:
+                maxLength=max(maxLength,right-left+1)
+
+            right+=1
+
+
+        return maxLength
+            
+
+        
+        
