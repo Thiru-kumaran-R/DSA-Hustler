cbfb1793f9416132b977fee695ae0b32168b030d
Ruchika Yadav <145842561+ruchika123yadav@users.noreply.github.com>
2026-07-13 23:22:17 +0530

    solved 2 two pointer question

--- Rath/SlidingWindow_TwoPointer/file.py
+++ Rath/SlidingWindow_TwoPointer/file.py
@@ -92,5 +92,73 @@ class Solution:
         return maxLength
             
 
+# LONGEST REAPEATING CHARACTER REPLACEMENT->https://leetcode.com/problems/longest-repeating-character-replacement/description/
+# COMPLEXITY=O(2n)
+    
+
+    class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+
+        hm={}
+
+        right=0
+        left=0
+        maxLen=0
+        maxFreq=0
+
+        while right<len(s):
+            hm[s[right]]=hm.get(s[right],0)+1
+            maxFreq=max(maxFreq,hm[s[right]])
+            
+            while (right-left+1)-maxFreq>k:
+                hm[s[left]]-=1
+                maxFreq=max(hm.values())
+                left+=1
+
+             
+            maxLen=max(maxLen,right-left+1)
+            right+=1
+
+        return maxLen
+
+
         
+# BINARY SUBARRAY WITH SUM->https://leetcode.com/problems/binary-subarrays-with-sum/description/
+# COMPLEXITY=O(2n)
+        
+
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+
+        if len(nums)==1 and (nums[0]>goal or nums[0]<goal):
+            return 0
+
+        
+        return self.subarr(nums,goal)-self.subarr(nums,goal-1)
+
+    
+    def subarr(self,nums: List[int], goal: int) -> int:
+
+        if goal<0:
+            return 0
+
+        left=0
+        right=0
+        preSum=0
+        count=0
+
+        while right<len(nums):
+
+            preSum+=nums[right]
+
+            while preSum>goal:
+                preSum-=nums[left]
+                left+=1
+
+            count+=right-left+1
+            right+=1
+
+        return count
+
+            
         
